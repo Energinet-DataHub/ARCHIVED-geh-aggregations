620aa20aba73afcebe8ac63c3ecb9a4c3693bda1
Kristian Schneider <khs@thamesconsulting.com>
2021-03-26 12:29:30 +0100

    Inital TF databricks workspace

---
+++ build/infrastructure/databricks-workspace.tf
@@ -0,0 +1,7 @@
+resource "azurerm_databricks_workspace" "databricks" {
+  name                = "dbw-${var.project}-${var.organisation}-${var.environment}"
+  resource_group_name                       = data.azurerm_resource_group.main.name
+  location                                  = data.azurerm_resource_group.main.location
+  sku                 = "standard"
+  tags                                      = data.azurerm_resource_group.main.tags
+}

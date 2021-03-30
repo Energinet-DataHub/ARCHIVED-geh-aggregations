272e8d8a066cfd922c01d6f09e9a462d0187d7fe
Kristian Schneider <khs@thamesconsulting.com>
2021-03-30 11:24:22 +0200

    To work with databricks_token.pat (orphan) its original provider configuration at provider["registry.terraform.io/databrickslabs/databricks"].created_workspace

--- build/infrastructure/databricks-workspace.tf
+++ build/infrastructure/databricks-workspace.tf
@@ -6,13 +6,10 @@ resource "azurerm_databricks_workspace" "databricksworkspace" {
   tags                = data.azurerm_resource_group.main.tags
 }
 
-# provider "databricks" {
-#   azure_client_id             = var.client_id
-#   azure_client_secret         = var.client_secret
-#   azure_tenant_id             = var.tenant_id
-#   alias = "created_workspace" 
-#   azure_workspace_resource_id = azurerm_databricks_workspace.databricksworkspace.id
-# }
+provider "databricks" {
+  alias = "created_workspace" 
+  azure_workspace_resource_id = azurerm_databricks_workspace.databricksworkspace.id
+}
 
 # // create PAT token to provision entities within workspace
 # resource "databricks_token" "pat" {

ed562e7817be349e955a88cacdbe7af5c9ead682
Kristian Schneider <khs@thamesconsulting.com>
2021-03-30 09:32:46 +0200

    secrets to databricks provider

--- build/infrastructure/databricks-workspace.tf
+++ build/infrastructure/databricks-workspace.tf
@@ -7,6 +7,9 @@ resource "azurerm_databricks_workspace" "databricksworkspace" {
 }
 
 provider "databricks" {
+  azure_client_id             = var.client_id
+  azure_client_secret         = var.client_secret
+  azure_tenant_id             = var.tenant_id
   alias = "created_workspace" 
   azure_workspace_resource_id = azurerm_databricks_workspace.databricksworkspace.id
 }

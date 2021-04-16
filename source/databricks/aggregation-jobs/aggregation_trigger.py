11b8f958cf167487fe520a0e4452d24434450bfb
jvm <jvm@energinet.dk>
2021-04-16 10:37:20 +0000

    Change naming

--- source/databricks/aggregation-jobs/aggregation_trigger.py
+++ source/databricks/aggregation-jobs/aggregation_trigger.py
@@ -107,8 +107,8 @@ added_grid_loss_df = calculate_added_grid_loss(grid_loss_df)
 # Join master data with CSV file from the task
 grid_loss_sys_cor_master_data_df = load_grid_sys_cor_master_data_dataframe(args, spark)
 
-cscmdwascdf = combine_added_system_correction_with_master_data(added_system_correction_df, grid_loss_sys_cor_master_data_df)
-cglmdwagldf = combine_added_grid_loss_with_master_data(added_grid_loss_df, grid_loss_sys_cor_master_data_df)
+combined_system_correction_df = combine_added_system_correction_with_master_data(added_system_correction_df, grid_loss_sys_cor_master_data_df)
+combined_grid_loss_df = combine_added_grid_loss_with_master_data(added_grid_loss_df, grid_loss_sys_cor_master_data_df)
 
 # STEP 10
 flex_consumption_with_grid_loss = adjust_flex_consumption(flex_consumption_df, added_grid_loss_df, grid_loss_sys_cor_master_data_df)
@@ -159,8 +159,8 @@ aggregation_results = AggregationResults(net_exchange_per_neighbour_df.toJSON().
                                         flex_consumption_df.toJSON().collect(),
                                         flex_consumption_with_grid_loss.toJSON().collect(),
                                         hourly_production_with_system_correction_and_grid_loss.toJSON().collect(),
-                                        cscmdwascdf.toJSON().collect(),
-                                        cglmdwagldf.toJSON().collect())
+                                        combined_system_correction_df.toJSON().collect(),
+                                        combined_grid_loss_df.toJSON().collect())
 
 
 coordinator_service = CoordinatorService(args)
--- source/databricks/geh_stream/DTOs/AggregationResults.py
+++ source/databricks/geh_stream/DTOs/AggregationResults.py
@@ -17,15 +17,15 @@ import json
 
 class AggregationResults:
 
-    def __init__(self, net_exchange_per_neighbour, hourlyConsumption, hourlyProduction, flexConsumption, flex_consumption_with_system_correction_and_grid_loss, hourly_production_with_system_correction_and_grid_loss, added_system_correction, added_grid_loss):
+    def __init__(self, net_exchange_per_neighbour, hourlyConsumption, hourlyProduction, flexConsumption, flex_consumption_with_system_correction_and_grid_loss, hourly_production_with_system_correction_and_grid_loss, combined_system_correction, combined_grid_loss):
         self.netExchangePerNeighbour = net_exchange_per_neighbour
         self.HourlyConsumption = hourlyConsumption
         self.HourlyProduction = hourlyProduction
         self.FlexConsumption = flexConsumption
         self.AdjustedFlexConsumption = flex_consumption_with_system_correction_and_grid_loss
         self.AdjustedHourlyProduction = hourly_production_with_system_correction_and_grid_loss
-        self.AddedSystemCorrection = added_system_correction
-        self.AddedGridLoss = added_grid_loss
+        self.CombinedSystemCorrection = combined_system_correction
+        self.CombinedGridLoss = combined_grid_loss
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)

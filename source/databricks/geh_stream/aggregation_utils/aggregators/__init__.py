54644dc73d08de1404f04a6556117b533cf7098a
jvm <jvm@energinet.dk>
2021-04-20 10:51:34 +0000

    Streamline of init file

--- source/databricks/geh_stream/aggregation_utils/aggregators/__init__.py
+++ source/databricks/geh_stream/aggregation_utils/aggregators/__init__.py
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from .aggregation_initializer import initialize_spark, load_grid_sys_cor_master_data_dataframe, load_timeseries_dataframe
+from .aggregation_initializer import initialize_spark, \
+    load_grid_sys_cor_master_data_dataframe, \
+    load_timeseries_dataframe
 from .aggregators import aggregate_net_exchange_per_ga, \
     aggregate_net_exchange_per_neighbour_ga, \
     aggregate_hourly_consumption, \

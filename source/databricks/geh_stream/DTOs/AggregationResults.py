90167a7b2ea3820d51647f61fe065dc12e274c42
jvm <jvm@energinet.dk>
2021-04-20 10:39:54 +0000

    Fix naming convention

--- source/databricks/geh_stream/DTOs/AggregationResults.py
+++ source/databricks/geh_stream/DTOs/AggregationResults.py
@@ -17,11 +17,11 @@ import json
 
 class AggregationResults:
 
-    def __init__(self, net_exchange_per_neighbour, hourlyConsumption, hourlyProduction, flexConsumption, flex_consumption_with_system_correction_and_grid_loss, hourly_production_with_system_correction_and_grid_loss, combined_system_correction, combined_grid_loss):
+    def __init__(self, net_exchange_per_neighbour, hourly_consumption, hourly_production, flex_consumption, flex_consumption_with_system_correction_and_grid_loss, hourly_production_with_system_correction_and_grid_loss, combined_system_correction, combined_grid_loss):
         self.netExchangePerNeighbour = net_exchange_per_neighbour
-        self.HourlyConsumption = hourlyConsumption
-        self.HourlyProduction = hourlyProduction
-        self.FlexConsumption = flexConsumption
+        self.HourlyConsumption = hourly_consumption
+        self.HourlyProduction = hourly_production
+        self.FlexConsumption = flex_consumption
         self.AdjustedFlexConsumption = flex_consumption_with_system_correction_and_grid_loss
         self.AdjustedHourlyProduction = hourly_production_with_system_correction_and_grid_loss
         self.CombinedSystemCorrection = combined_system_correction

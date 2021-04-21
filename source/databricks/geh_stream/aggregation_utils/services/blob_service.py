9392bb211ac92a98922f8f2ebdc608c2f81c193f
Kristian Schneider <khs@thamesconsulting.com>
2021-04-21 11:44:55 +0200

    Configured connectionString in upload_blob

--- source/databricks/geh_stream/aggregation_utils/services/blob_service.py
+++ source/databricks/geh_stream/aggregation_utils/services/blob_service.py
@@ -20,13 +20,17 @@ import json
 
 class BlobService:
 
+    def __init__(self, args):
+        self.CONNECTIONSTRING = "DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1};EndpointSuffix=core.windows.net".format(args.input_storage_account_name, args.input_storage_account_key)
+        self.containerName = args.input_storage_container_name
+
     def upload_blob(self, data, blob_name):
 
         jsonObj = data.toJSON().collect()
         jsonStr = json.dumps(jsonObj)
         snappyData = snappy.compress(jsonStr)
-        blob_service_client = BlobServiceClient.from_connection_string("DefaultEndpointsProtocol=https;AccountName=timeseriesdatajouless;AccountKey=KIGkX7XIGilmUwADO5qT7k4AzFk8S0nl5QmfBD3+ktHxqhoVwfQUPeV7DpMxvhmTNVe/bcdJwA6PIyUHuvqKZw==;EndpointSuffix=core.windows.net")
-        blob_client = blob_service_client.get_blob_client(container="messagedata", blob=blob_name)
+        blob_service_client = BlobServiceClient.from_connection_string(self.CONNECTIONSTRING)
+        blob_client = blob_service_client.get_blob_client(container=self.containerName, blob=blob_name)
         try:
             blob_client.get_blob_properties()
             blob_client.delete_blob()

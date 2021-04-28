3eca35680cb8d34c558b292c600c85d3056a2362
Jonas Michelsen <jvm@energinet.dk>
2021-04-28 12:58:45 +0200

    wip - CombinedGridLossStrategy.cs

---
+++ source/coordinator/GreenEnergyHub.Aggregation.Application/Coordinator/Strategies/CombinedGridLossStrategy.cs
@@ -0,0 +1,81 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using GreenEnergyHub.Aggregation.Application.Services;
+using GreenEnergyHub.Aggregation.Domain;
+using GreenEnergyHub.Aggregation.Domain.DTOs;
+using GreenEnergyHub.Aggregation.Domain.Types;
+using GreenEnergyHub.Aggregation.Infrastructure;
+using GreenEnergyHub.Aggregation.Infrastructure.ServiceBusProtobuf;
+using GreenEnergyHub.Messaging.Transport;
+using Microsoft.Extensions.Logging;
+using NodaTime.Text;
+
+namespace GreenEnergyHub.Aggregation.Application.Coordinator.Strategies
+{
+    public class CombinedGridLossStrategy : BaseStrategy<AdjustedFlexConsumption>, IDispatchStrategy
+    {
+        private readonly IGLNService _glnService;
+        private readonly ISpecialMeteringPointsService _specialMeteringPointsService;
+
+        public CombinedGridLossStrategy(
+            IGLNService glnService,
+            ISpecialMeteringPointsService specialMeteringPointsService,
+            ILogger<AdjustedFlexConsumption> logger,
+            Dispatcher dispatcher)
+        : base(logger, dispatcher)
+        {
+            _glnService = glnService;
+            _specialMeteringPointsService = specialMeteringPointsService;
+        }
+
+        public string FriendlyNameInstance => "combined_grid_loss";
+
+        public override IEnumerable<IOutboundMessage> PrepareMessages(
+            IEnumerable<AdjustedFlexConsumption> list,
+            ProcessType processType,
+            string timeIntervalStart,
+            string timeIntervalEnd)
+        {
+            var validTime = InstantPattern.ExtendedIso.Parse(timeIntervalStart).GetValueOrThrow();
+
+            return (from energySupplier in list.GroupBy(hc => hc.EnergySupplierMarketParticipantMRID)
+                    from gridArea in energySupplier.GroupBy(e => e.MeteringGridAreaDomainMRID)
+                    let first = gridArea.First()
+                    where _specialMeteringPointsService.GridLossOwner(first.MeteringGridAreaDomainMRID, validTime) == first.EnergySupplierMarketParticipantMRID
+                    select new AggregatedMeteredDataTimeSeries(CoordinatorSettings.AdjustedFlexConsumptionName)
+                    {
+                        MeteringGridAreaDomainMRid = first.MeteringGridAreaDomainMRID,
+                        BalanceResponsiblePartyMarketParticipantMRid = first.BalanceResponsiblePartyMarketParticipantMRID,
+                        BalanceSupplierPartyMarketParticipantMRid = first.EnergySupplierMarketParticipantMRID,
+                        MarketEvaluationPointType = MarketEvaluationPointType.Consumption,
+                        SettlementMethod = SettlementMethodType.FlexSettled,
+                        ProcessType = Enum.GetName(typeof(ProcessType), processType),
+                        Quantities = gridArea.Select(e => e.SumQuantity),
+                        TimeIntervalStart = timeIntervalStart,
+                        TimeIntervalEnd = timeIntervalEnd,
+                        ReceiverMarketParticipantMRid = _glnService.GetGlnFromSupplierId(first.EnergySupplierMarketParticipantMRID),
+                        SenderMarketParticipantMRid = _glnService.GetSenderGln(),
+                    }).Cast<IOutboundMessage>()
+                .ToList();
+        }
+    }
+}
---
+++ source/coordinator/GreenEnergyHub.Aggregation.Domain/DTOs/CombinedGridLoss.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace GreenEnergyHub.Aggregation.Domain.DTOs
+{
+    public class CombinedGridLoss
+    {
+        [JsonProperty("BalanceResponsibleParty_MarketParticipant_mRID")]
+        public string BalanceResponsiblePartyMarketParticipantMRid { get; set; }
+
+        [JsonProperty("ConnectionState")]
+        public string ConnectionState { get; set; }
+
+        [JsonProperty("IsGridLoss")]
+        public bool IsGridLoss { get; set; }
+
+        [JsonProperty("IsSystemCorrection")]
+        public bool IsSystemCorrection { get; set; }
+
+        [JsonProperty("MarketEvaluationPointType")]
+        public string MarketEvaluationPointType { get; set; }
+
+        [JsonProperty("MarketEvaluationPoint_mRID")]
+        public string MarketEvaluationPointMRid { get; set; }
+
+        [JsonProperty("MeterReadingPeriodicity")]
+        public string MeterReadingPeriodicity { get; set; }
+
+        [JsonProperty("MeteringGridArea_Domain_mRID")]
+        public string MeteringGridAreaDomainMRid { get; set; }
+
+        [JsonProperty("MeteringMethod")]
+        public string MeteringMethod { get; set; }
+
+        [JsonProperty("SettlementMethod")]
+        public string SettlementMethod { get; set; }
+
+        [JsonProperty("ValidFrom")]
+        public DateTimeOffset ValidFrom { get; set; }
+
+        [JsonProperty("added_system_correction")]
+        public double AddedSystemCorrection { get; set; }
+
+        [JsonProperty("time_window")]
+        public TimeWindow TimeWindow { get; set; }
+    }
+}

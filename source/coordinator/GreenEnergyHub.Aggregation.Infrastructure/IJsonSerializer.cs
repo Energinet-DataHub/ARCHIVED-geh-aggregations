f0cfb6427981ccf3129c43a7e914d94dad1294d9
Jonas Michelsen <jvm@energinet.dk>
2021-05-11 13:27:08 +0200

    Create wrapper around static system.text.json.jsonSerializer So we are able to make custom options

---
+++ source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/IJsonSerializer.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GreenEnergyHub.Aggregation.Infrastructure
+{
+    /// <summary>
+    /// IJsonSerializer wrapper for system.text.json.JsonSerializer
+    /// </summary>
+    public interface IJsonSerializer
+    {
+        /// <summary>
+        /// Read the UTF-8 encoded text representing a single JSON value into a <typeparamref name="TValue"/>.
+        /// The Stream will be read to completion.
+        /// </summary>
+        /// <returns>A <typeparamref name="TValue"/> representation of the JSON value.</returns>
+        /// <param name="utf8Json">JSON data to parse.</param>
+        /// <param name="cancellationToken">
+        /// The <see cref="System.Threading.CancellationToken"/> which may be used to cancel the read operation.
+        /// </param>
+        /// <exception cref="JsonException">
+        /// Thrown when the JSON is invalid,
+        /// <typeparamref name="TValue"/> is not compatible with the JSON,
+        /// or when there is remaining data in the Stream.
+        /// </exception>
+        public ValueTask<TValue> DeserializeAsync<TValue>(
+            Stream utf8Json,
+            CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Convert the provided value into a <see cref="string"/>.
+        /// </summary>
+        /// <returns>A <see cref="string"/> representation of the value.</returns>
+        /// <param name="value">The value to convert.</param>
+        /// <remarks>Using a <see cref="string"/> is not as efficient as using UTF-8
+        /// encoding since the implementation internally uses UTF-8. See also <see cref="SerializeToUtf8Bytes"/>
+        /// and <see cref="SerializeAsync"/>.
+        /// </remarks>
+        public string Serialize<TValue>(TValue value);
+    }
+}
---
+++ source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/JsonSerializerWithOption.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using NodaTime;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace GreenEnergyHub.Aggregation.Infrastructure
+{
+    public class JsonSerializerWithOption : IJsonSerializer
+    {
+        private readonly JsonSerializerOptions _options;
+
+        public JsonSerializerWithOption()
+        {
+            _options = new JsonSerializerOptions().ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+        }
+
+        public ValueTask<TValue> DeserializeAsync<TValue>(
+            Stream utf8Json,
+            CancellationToken cancellationToken = default)
+        {
+            return JsonSerializer.DeserializeAsync<TValue>(utf8Json, _options, cancellationToken);
+        }
+
+        public string Serialize<TValue>(TValue value)
+        {
+            return JsonSerializer.Serialize(value, _options);
+        }
+    }
+}

9a6ec9d7159e8621678cc3f7c975cafdb8d08415
Morten Kjær Nicolaysen <71763962+mknic@users.noreply.github.com>
2021-05-12 11:25:51 +0200

    Update source/databricks/geh_stream/aggregation_utils/aggregators/adjust_production.py
    Co-authored-by: Lasse Klitgaard <lki@energinet.dk>

--- source/databricks/geh_stream/aggregation_utils/aggregators/adjust_production.py
+++ source/databricks/geh_stream/aggregation_utils/aggregators/adjust_production.py
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import col, when, lit
-from geh_stream.codelists import Quality
+from pyspark.sql.functions import col, when
 
 
 # step 11

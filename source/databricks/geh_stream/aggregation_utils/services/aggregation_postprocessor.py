eb8f5752eac1ab553cfdfb1cc4f92bf4492ae0eb
Kristian Schneider <khs@thamesconsulting.com>
2021-05-18 13:58:59 +0200

    .

--- source/databricks/geh_stream/aggregation_utils/services/aggregation_postprocessor.py
+++ source/databricks/geh_stream/aggregation_utils/services/aggregation_postprocessor.py
@@ -38,5 +38,6 @@ class PostProcessor:
 
         if args.persist_source_dataframe:
             snapshot_path = "abfss://{0}@{1}.dfs.core.windows.net/{2}/{3}".format(args.input_storage_container_name, args.input_storage_account_name, args.persist_source_dataframe_location, now_path_string)
+            print("We are snapshotting " + filtered.count() + " dataframes to " + snapshot_path)
             filtered.write.option("compression", "snappy").save(snapshot_path)
             self.coordinator_service.notify_snapshot_coordinator(snapshot_path)

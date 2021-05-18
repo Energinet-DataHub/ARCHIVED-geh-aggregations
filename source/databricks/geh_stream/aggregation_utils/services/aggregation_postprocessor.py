b36607965b28f4bf56e5939bd942907d2dd697fc
Kristian Schneider <khs@thamesconsulting.com>
2021-05-18 11:36:28 +0200

    .

--- source/databricks/geh_stream/aggregation_utils/services/aggregation_postprocessor.py
+++ source/databricks/geh_stream/aggregation_utils/services/aggregation_postprocessor.py
@@ -23,22 +23,21 @@ class PostProcessor:
     def __init__(self, args):
         self.coordinator_service = CoordinatorService(args)
         self.blob_service = BlobService(args)
+        self.now_path_string = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-    def now_path_string(self):
-        return datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
     def do_post_processing(self, args, results):
 
         result_path = "Results"
 
         for key, value in results.items():
-            path = "{0}/{1}/{2}.json.gz".format(result_path, self.now_path_string(), key)
+            path = "{0}/{1}/{2}.json.gz".format(result_path, self.now_path_string, key)
             self.blob_service.upload_blob(value, path)
             self.coordinator_service.notify_coordinator(path)
 
     def store_basis_data(self, args, filtered):
 
         if args.persist_source_dataframe:
-            snapshot_path = "abfss://{0}@{1}.dfs.core.windows.net/{2}/{3}".format(args.input_storage_container_name, args.input_storage_account_name, args.persist_source_dataframe_location, self.now_path_string())
+            snapshot_path = "abfss://{0}@{1}.dfs.core.windows.net/{2}/{3}".format(args.input_storage_container_name, args.input_storage_account_name, args.persist_source_dataframe_location, self.now_path_string)
             filtered.write.option("compression", "snappy").save(snapshot_path)
             self.coordinator_service.notify_snapshot_coordinator(snapshot_path)

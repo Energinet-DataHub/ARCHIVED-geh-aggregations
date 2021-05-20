71b7065b65c5a78c03932cbef9f6d44fa525b289
Kristian Schneider <khs@thamesconsulting.com>
2021-05-20 08:23:52 +0200

    added results['grid_loss_df']

--- source/databricks/aggregation-jobs/aggregation_trigger.py
+++ source/databricks/aggregation-jobs/aggregation_trigger.py
@@ -102,16 +102,16 @@ results['flex_consumption_df'] = aggregate_flex_consumption(df)
 results['hourly_production_df'] = aggregate_hourly_production(df)
 
 # STEP 6
-grid_loss_df = calculate_grid_loss(results['net_exchange_per_ga_df'],
+results['grid_loss_df'] = calculate_grid_loss(results['net_exchange_per_ga_df'],
                                               results['hourly_consumption_df'],
                                               results['flex_consumption_df'],
                                               results['hourly_production_df'])
 
 # STEP 8
-added_system_correction_df = calculate_added_system_correction(grid_loss_df)
+added_system_correction_df = calculate_added_system_correction(results['grid_loss_df'])
 
 # STEP 9
-added_grid_loss_df = calculate_added_grid_loss(grid_loss_df)
+added_grid_loss_df = calculate_added_grid_loss(results['grid_loss_df'])
 
 # Get additional data for grid loss and system correction
 grid_loss_sys_cor_master_data_df = load_grid_sys_cor_master_data_dataframe(args, spark)

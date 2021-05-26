b896258eba5cdf2cefb14c92addc4b8cdefd52cc
Finn Skødt Toft <fkt@energinet.dk>
2021-05-26 14:11:28 +0200

    Feature/128189 metadata schema (#108)
    * Initial database migration
    
    * initial pipeline test
    
    * removed func deploy
    
    * .
    
    * .
    
    * ref to migration
    
    * mig test
    
    * mig2
    
    * mir3
    
    * mir4
    
    * kv1
    
    * Clean upfor PR
    
    * Implemented connection string as first parameter to the Database Migration Project, insted of hardcoded value
    
    * Added copyright to files
    
    Co-authored-by: Kristian Schneider <khs@thamesconsulting.com>

---
+++ source/coordinator/GreenEnergyHub.Aggregation.DatabaseMigration/Program.cs
@@ -0,0 +1,80 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GreenEnergyHub.Aggregation.DatabaseMigration
+{
+    internal static class Program
+    {
+        private static string _connectionString;
+
+        private static void Main(string[] args)
+        {
+            // The first argument is the connection string
+            _connectionString = args.FirstOrDefault();
+
+            if (_connectionString == null)
+            {
+                Console.WriteLine("You need to provide a connection string as the first parameter");
+                return;
+            }
+
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString(_connectionString)
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(JobsTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}

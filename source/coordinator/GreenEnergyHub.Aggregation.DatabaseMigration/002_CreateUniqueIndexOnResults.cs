9c805ecef9e803656a7dd0dd0077779fbc5d2998
Finn Skødt Toft <fkt@energinet.dk>
2021-05-26 15:21:02 +0200

    Added Unique Constraint on Results table. Some refactor of migration file names

---
+++ source/coordinator/GreenEnergyHub.Aggregation.DatabaseMigration/002_CreateUniqueIndexOnResults.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+
+namespace GreenEnergyHub.Aggregation.DatabaseMigration
+{
+    [Migration(2)]
+    public class CreateUniqueIndexOnResults : Migration
+    {
+        public override void Up()
+        {
+            Create.UniqueConstraint("UQ_Results_JobId_Name").OnTable("Results").Columns("JobId", "Name");
+        }
+
+        public override void Down()
+        {
+            Delete.UniqueConstraint("UQ_Results_JobId_Name");
+        }
+    }
+}

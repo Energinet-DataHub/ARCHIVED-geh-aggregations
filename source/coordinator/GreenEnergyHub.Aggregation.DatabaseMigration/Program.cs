43ca5569176f51b2696d0f7b41714b414165dc68
Finn Skødt Toft <fkt@energinet.dk>
2021-05-26 10:07:22 +0200

    Initial database migration

---
+++ source/coordinator/GreenEnergyHub.Aggregation.DatabaseMigration/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GreenEnergyHub.Aggregation.DatabaseMigration
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </summary>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer()
+                    // Set the connection string
+                    .WithGlobalConnectionString(@"Server=DESKTOP-9QVUFMD\SQLEXPRESS;Database=TestDb;Integrated Security=true;")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(JobsTable).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </summary>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}

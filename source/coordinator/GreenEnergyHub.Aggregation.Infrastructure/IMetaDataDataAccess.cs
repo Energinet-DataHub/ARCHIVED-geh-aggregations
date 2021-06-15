1b0e32830aefc43ef844f0f25f950518f312da18
Finn Skødt Toft <fkt@energinet.dk>
2021-06-15 15:17:11 +0200

    Added documentation to interface

--- source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/IMetaDataDataAccess.cs
+++ source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/IMetaDataDataAccess.cs
@@ -8,12 +8,32 @@ namespace GreenEnergyHub.Aggregation.Infrastructure
     /// </summary>
     public interface IMetaDataDataAccess
     {
+        /// <summary>
+        /// Insert Job
+        /// </summary>
+        /// <param name="job"></param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         Task CreateJobAsync(Job job);
 
+        /// <summary>
+        /// Update Job
+        /// </summary>
+        /// <param name="job"></param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         Task UpdateJobAsync(Job job);
 
+        /// <summary>
+        /// Insert job
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         Task CreateResultItemAsync(Result result);
 
+        /// <summary>
+        /// Update job
+        /// </summary>
+        /// <param name="result"></param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         Task UpdateResultItemAsync(Result result);
     }
 }

9e5a36bd1006b75bb865fdad126b1f0b5edb24ad
Morten Kjær Nicolaysen <71763962+mknic@users.noreply.github.com>
2021-06-22 15:10:02 +0200

    add timeseries collection to masterdata db

--- build/infrastructure/cosmos-master-data.tf
+++ build/infrastructure/cosmos-master-data.tf
@@ -45,3 +45,12 @@ resource "azurerm_cosmosdb_sql_container" "collection_meteringpoints" {
   database_name       = azurerm_cosmosdb_sql_database.db.name
   partition_key_path  = "/recipient"
 }
+
+resource "azurerm_cosmosdb_sql_container" "collection_timeseries" {
+  name                = "timeseries"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.masterdata.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path  = "/recipient"
+}
+

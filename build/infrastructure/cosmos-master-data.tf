89cdc7e68b24c441ff092bb14f06607d9401b5af
Jonas Michelsen <jvm@energinet.dk>
2021-06-23 11:19:42 +0000

    Add chargelinks to cosmos with terraform

--- build/infrastructure/cosmos-master-data.tf
+++ build/infrastructure/cosmos-master-data.tf
@@ -69,3 +69,11 @@ resource "azurerm_cosmosdb_sql_container" "collection_charges" {
   database_name       = azurerm_cosmosdb_sql_database.db.name
   partition_key_path  = "/recipient"
 }
+
+resource "azurerm_cosmosdb_sql_container" "collection_chargelinks" {
+  name                = "chargelinks"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.masterdata.name
+  database_name       = azurerm_cosmosdb_sql_database.db.name
+  partition_key_path  = "/recipient"
+}

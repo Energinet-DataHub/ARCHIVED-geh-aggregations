6388a273a13fe59246467369bff8db5f2ac6ac01
Lasse Klitgaard <klitgaard84@gmail.com>
2021-06-23 14:09:48 +0200

    add unit test

---
+++ source/coordinator/GreenEnergyHub.Aggregation.Domain/DTOs/MeteringPoint.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenEnergyHub.Aggregation.Domain.DTOs
+{
+    public class MeteringPoint
+    {
+        public string MeteringPointId { get; set; }
+
+        public string MeteringPointType { get; set; }
+
+        public string SettlementMethod { get; set; }
+
+        public bool Connected { get; set; }
+
+        public DateTime ValidFrom { get; set; }
+
+        public DateTime? ValidTo { get; set; }
+    }
+}

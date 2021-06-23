43fe0c250152279a18ca0d5bdf59b1364cee44bd
Lasse Klitgaard <klitgaard84@gmail.com>
2021-06-23 14:12:04 +0200

    forgot to save

--- source/coordinator/GreenEnergyHub.Aggregation.Tests/EventSourcingTests.cs
+++ source/coordinator/GreenEnergyHub.Aggregation.Tests/EventSourcingTests.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using GreenEnergyHub.Aggregation.Domain.DTOs;
+using Xunit;
 
 namespace GreenEnergyHub.Aggregation.Tests
 {
-    class EventSourcingTests
+    public class EventSourcingTests
     {
+        [Fact]
+        public void OnlyCreatedEventTest()
+        {
+            var events = new List<IEvent>();
+
+            var createdEvent = new MeteringPointCreatedEvent("1")
+            {
+                Connected = false,
+                EffectuationDate = "2020-01-01",
+                MeteringPointType = "E17",
+                SettlementMethod = "D01",
+            };
+
+            events.Add(createdEvent);
+
+            var linkedList = new LinkedList<MeteringPoint>();
+
+
+        }
     }
 }

52511d97c9b1cc15828d16d57448e8c9974f168b
Kristian Schneider <khs@thamesconsulting.com>
2021-06-29 12:12:43 +0200

    .

---
+++ source/test-data-generation/GreenEnergyHub.Aggregation.TestData.Tests/Dummy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace GreenEnergyHub.Aggregation.TestData.Tests
+{
+    [Trait("Category", "Component")]
+    public class DummyTests
+    {
+        [Fact]
+        public void Dummy()
+        {
+            Assert.True(true);
+        }
+    }
+}

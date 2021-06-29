de6982c9465c51a30913bd1f15381bf4fc040e13
Kristian Schneider <khs@thamesconsulting.com>
2021-06-29 10:18:04 +0200

    Scafolding, Ci and CD for generator

---
+++ source/coordinator/GreenEnergyHub.Aggregation.TestData.Generator/TestDataGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace GreenEnergyHub.Aggregation.TestData.Generator
+{
+    public static class TestDataGenerator
+    {
+        [FunctionName("BlobTrigger")]
+        public static void Run([BlobTrigger("test-data-source/{name}", Connection = "TEST_DATA_SOURCE_CONNECTION_STRING")]Stream myBlob, string name, ILogger log)
+        {
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}

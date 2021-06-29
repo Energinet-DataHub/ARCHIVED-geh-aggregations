8ab60e79c3a040208747485979219565313bddc7
Kristian Schneider <khs@thamesconsulting.com>
2021-06-29 14:06:18 +0200

    MasterDataStorage

---
+++ source/test-data-generation/GreenEnergyHub.Aggregation.TestData.Infrastructure/CosmosDb/MasterDataStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreenEnergyHub.Aggregation.TestData.Infrastructure.CosmosDb
+{
+    public class MasterDataStorage : IMasterDataStorage
+    {
+        private string _baseConnectionString;
+
+        public MasterDataStorage(GeneratorSettings generatorSettings)
+        {
+            _baseConnectionString = generatorSettings.MasterDataStorageConnectionString;
+        }
+    }
+}

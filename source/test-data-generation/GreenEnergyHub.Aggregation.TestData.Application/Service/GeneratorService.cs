f530eacb138a2a801833537ce06771b453bc1e53
Kristian Schneider <khs@thamesconsulting.com>
2021-06-30 09:41:31 +0200

    lint

--- source/test-data-generation/GreenEnergyHub.Aggregation.TestData.Application/Service/GeneratorService.cs
+++ source/test-data-generation/GreenEnergyHub.Aggregation.TestData.Application/Service/GeneratorService.cs
@@ -14,7 +14,6 @@ using System.IO;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Text;

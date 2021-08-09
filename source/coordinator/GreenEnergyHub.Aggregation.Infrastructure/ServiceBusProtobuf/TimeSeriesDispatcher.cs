650190330d0decad6b50def7c739b71bc9c60b4a
Kristian Schneider <khs@thamesconsulting.com>
2021-08-09 15:09:32 +0200

    Update source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/ServiceBusProtobuf/TimeSeriesDispatcher.cs
    Co-authored-by: Lasse Klitgaard <lki@energinet.dk>

--- source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/ServiceBusProtobuf/TimeSeriesDispatcher.cs
+++ source/coordinator/GreenEnergyHub.Aggregation.Infrastructure/ServiceBusProtobuf/TimeSeriesDispatcher.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using GreenEnergyHub.Aggregation.Application.Coordinator.Interfaces;
 using GreenEnergyHub.Messaging;
 using GreenEnergyHub.Messaging.Transport;
 

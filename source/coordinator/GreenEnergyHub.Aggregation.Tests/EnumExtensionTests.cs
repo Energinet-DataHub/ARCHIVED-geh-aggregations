9ad2d8147e25a37e3c6373450967820b5909a3a6
Kristian Schneider <khs@thamesconsulting.com>
2021-08-12 08:24:11 +0200

    Small test for enum extension

---
+++ source/coordinator/GreenEnergyHub.Aggregation.Tests/EnumExtensionTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using GreenEnergyHub.Aggregation.Application.Utilities;
+using Xunit;
+
+namespace GreenEnergyHub.Aggregation.Tests
+{
+    public enum TestEnum
+    {
+        [Description("zero")]
+        Zero = 0,
+        [Description("one")]
+        One = 1,
+    }
+
+    [Trait("Category", "Component")]
+    public class EnumExtensionTests
+    {
+        [Fact]
+        public void Check_getting_description_from_enum()
+        {
+            var first = TestEnum.Zero;
+            Assert.Equal("zero", first.GetDescription());
+            Assert.NotEqual("one", first.GetDescription());
+        }
+    }
+}

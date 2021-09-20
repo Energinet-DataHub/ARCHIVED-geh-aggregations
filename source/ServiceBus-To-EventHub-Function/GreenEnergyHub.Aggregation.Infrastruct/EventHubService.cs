b3f849b1e74ec8302a2978c469cd070222ad5490
Kristian Schneider <khs@thamesconsulting.com>
2021-09-20 19:48:09 +0200

    Convert protobuf to json for eventhub

---
+++ source/ServiceBus-To-EventHub-Function/GreenEnergyHub.Aggregation.Infrastruct/EventHubService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+
+namespace GreenEnergyHub.Aggregation.Infrastruct
+{
+    public class EventHubService
+    {
+        private EventHubProducerClient _producer;
+
+        public EventHubService(string eventHubConnectionString, string eventHubName)
+        {
+            _producer = new EventHubProducerClient(eventHubConnectionString, eventHubName);
+        }
+
+        public async Task SendEventHubTestMessage(string message)
+        {
+            try
+            {
+                using EventDataBatch eventBatch = await _producer.CreateBatchAsync().ConfigureAwait(false);
+                Console.WriteLine("Converting message");
+                var eventBody = new BinaryData(message);
+                var eventData = new EventData(eventBody);
+
+                if (!eventBatch.TryAdd(eventData))
+                {
+                    Console.WriteLine("Failed adding message to batch");
+                    // At this point, the batch is full but our last event was not
+                    // accepted.  For our purposes, the event is unimportant so we
+                    // will intentionally ignore it.  In a real-world scenario, a
+                    // decision would have to be made as to whether the event should
+                    // be dropped or published on its own.
+                }
+
+                // When the producer publishes the event, it will receive an
+                // acknowledgment from the Event Hubs service; so long as there is no
+                // exception thrown by this call, the service assumes responsibility for
+                // delivery.  Your event data will be published to one of the Event Hub
+                // partitions, though there may be a (very) slight delay until it is
+                // available to be consumed.
+                Console.WriteLine("Sending message");
+                await _producer.SendAsync(eventBatch).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed sending event hub message " + e.Message);
+                // Transient failures will be automatically retried as part of the
+                // operation. If this block is invoked, then the exception was either
+                // fatal or all retries were exhausted without a successful publish.
+            }
+        }
+    }
+}
--- source/ServiceBus-To-EventHub-Function/GreenEnergyHub.Aggregation.ServiceBusToEvtHubFunction/ServiceBusListenerFunction.cs
+++ source/ServiceBus-To-EventHub-Function/GreenEnergyHub.Aggregation.ServiceBusToEvtHubFunction/ServiceBusListenerFunction.cs
@@ -2,21 +2,35 @@
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using GreenEnergyHub.Aggregation.Domain;
+using GreenEnergyHub.Aggregation.Infrastruct;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace GreenEnergyHub.Aggregation.ServiceBusToEvtHubFunction
 {
-    public static class ServiceBusListenerFunction
+    public class ServiceBusListenerFunction
     {
+        private readonly IConfiguration _configuration;
+
+        public ServiceBusListenerFunction(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [Function("ServiceBusListenerFunction")]
-        public static void Run([ServiceBusTrigger("joules-topic", "joules-sb-subscription", Connection = "ServiceBusConnection")] byte[] msgData, FunctionContext context)
+        public async Task RunAsync([ServiceBusTrigger("joules-topic", "joules-sb-subscription", Connection = "ServiceBusConnection")] byte[] msgData, FunctionContext context)
         {
             var logger = context.GetLogger("ServiceBusListenerFunction");
             //logger.LogInformation($"C# ServiceBus queue trigger function processed message: {item}");
             var obj = ChargeLinkCreatedContract.Parser.ParseFrom(msgData);
             logger.LogInformation($"charge id is {obj.ChargeId}");
+            var sender = new EventHubService(
+                _configuration["EventHubConnectionStringSender"],
+                _configuration["EventHubName"]);
+            await sender.SendEventHubTestMessage(System.Text.Json.JsonSerializer.Serialize(obj)).ConfigureAwait(false);
         }
     }
 }

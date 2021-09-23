d085ae507e21e4242f2349b93e47877ae2c622a7
Kristian Schneider <khs@thamesconsulting.com>
2021-09-23 09:01:16 +0200

    Service bus to event hub function (#369)
    * Initial work
    
    * Convert protobuf to json for eventhub
    
    * Initial scaffolding
    
    * Implemented mapper for protobuf contracts
    
    * Raw forwarding of message data
    
    * Renamed
    
    * Rename path
    
    * Deployment
    
    * Licenses
    
    * lic
    
    * .
    
    * KV connection strings to shared servicebus
    
    * yaml syntax
    
    * Create eventhub
    
    * wire evt hub up

---
+++ build/infrastructure/evh-aggregation.tf
@@ -0,0 +1,59 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+module "evhnm_aggregation" {
+  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//event-hub-namespace?ref=1.0.0"
+  name                      = "evhnm-aggregation-${var.project}-${var.organisation}-${var.environment}"
+  resource_group_name       = data.azurerm_resource_group.main.name
+  location                  = data.azurerm_resource_group.main.location
+  sku                       = "Standard"
+  capacity                  = 1
+  tags                      = data.azurerm_resource_group.main.tags
+}
+
+module "evh_aggregation" {
+  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//event-hub?ref=1.0.0"
+  name                      = "evh-aggregation"
+  namespace_name            = module.evhnm_aggregation.name
+  resource_group_name       = data.azurerm_resource_group.main.name
+  partition_count           = 4
+  message_retention         = 1
+  dependencies              = [
+    module.evhnm_aggregation
+  ]
+}
+
+module "evhar_aggregation_sender" {
+  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//event-hub-auth-rule?ref=1.0.0"
+  name                      = "evhar-aggregation-sender"
+  namespace_name            = module.evhnm_aggregation.name
+  eventhub_name             = module.evh_aggregation.name
+  resource_group_name       = data.azurerm_resource_group.main.name
+  send                      = true
+  dependencies              = [
+    module.evh_aggregation
+  ]
+}
+
+module "evhar_aggregation_listener" {
+  source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//event-hub-auth-rule?ref=1.0.0"
+  name                      = "evhar-aggregation-listener"
+  namespace_name            = module.evhnm_aggregation.name
+  eventhub_name             = module.evh_aggregation.name
+  resource_group_name       = data.azurerm_resource_group.main.name
+  listen                    = true
+  dependencies              = [
+    module.evh_aggregation
+  ]
+}
---
+++ source/ServiceBus-To-EventHub-Dispatcher/GreenEnergyHub.Aggregation.Infrastruct/EventHubService.cs
@@ -0,0 +1,105 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using GreenEnergyHub.Aggregation.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GreenEnergyHub.Aggregation.Infrastruct
+{
+    public class EventHubService : IEventHubService, IDisposable, IAsyncDisposable
+    {
+        private readonly ILogger<EventHubService> _logger;
+        private EventHubProducerClient _producerClient;
+
+        public EventHubService(IConfiguration configuration, ILogger<EventHubService> logger)
+        {
+            if (configuration == null)
+            {
+                throw new ArgumentException($"configuration is null");
+            }
+
+            _logger = logger;
+
+            _producerClient = new EventHubProducerClient(
+                configuration["EventHubConnectionStringSender"], configuration["EventHubName"]);
+        }
+
+        public async Task SendEventHubMessageAsync(byte[] msg)
+        {
+            try
+            {
+                using var eventBatch = await _producerClient.CreateBatchAsync().ConfigureAwait(false);
+
+                _logger.LogInformation("Sending message onto eventhub");
+                var eventBody = new BinaryData(msg);
+                var eventData = new EventData(eventBody);
+
+                if (!eventBatch.TryAdd(eventData))
+                {
+                    _logger.LogError("Failed adding message to batch");
+                }
+
+                await _producerClient.SendAsync(eventBatch).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError("Failed sending event hub message " + e.Message);
+                // Transient failures will be automatically retried as part of the
+                // operation. If this block is invoked, then the exception was either
+                // fatal or all retries were exhausted without a successful publish.
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsyncCore();
+
+            Dispose(disposing: false);
+#pragma warning disable CA1816 // Dispose methods should call SuppressFinalize
+            GC.SuppressFinalize(this);
+#pragma warning restore CA1816 // Dispose methods should call SuppressFinalize
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                (_producerClient as IDisposable)?.Dispose();
+            }
+
+            _producerClient = null;
+        }
+
+        protected virtual async ValueTask DisposeAsyncCore()
+        {
+            if (_producerClient != null)
+            {
+                await _producerClient.DisposeAsync().ConfigureAwait(false);
+            }
+
+            _producerClient = null;
+        }
+    }
+}

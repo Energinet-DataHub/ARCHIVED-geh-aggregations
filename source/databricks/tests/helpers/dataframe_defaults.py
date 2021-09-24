8765e4e5a9ef4afcea9649c018a6eee197a6c78e
Jonas Michelsen <jvm@energinet.dk>
2021-09-24 12:53:44 +0000

    Add connected as default for dataframe creator

--- source/databricks/tests/helpers/dataframe_defaults.py
+++ source/databricks/tests/helpers/dataframe_defaults.py
@@ -31,7 +31,7 @@ class DataframeDefaults():
     default_charge_price: Decimal = Decimal("1.123456")
     default_charge_tax: str = "true"
     default_charge_type: str = const_charge_type
-    default_connection_state: str = "1"
+    default_connection_state: str = "E22"
     default_currency: str = "DDK"
     default_energy_supplier_id: str = "1"
     default_grid_area: str = "chargea"

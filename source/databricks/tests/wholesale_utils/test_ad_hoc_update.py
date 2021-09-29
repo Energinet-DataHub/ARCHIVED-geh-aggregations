71658e4d9f183d092d678ff20e1bccfbe1f0a6a7
Morten Kjær Nicolaysen <71763962+mknic@users.noreply.github.com>
2021-09-29 12:46:26 +0200

    split event dataframe into multiple dataframes

--- source/databricks/tests/wholesale_utils/test_ad_hoc_update.py
+++ source/databricks/tests/wholesale_utils/test_ad_hoc_update.py
@@ -16,6 +16,7 @@ from datetime import datetime
 import pytest
 import pandas as pd
 from pyspark.sql.types import StructType, StringType, StructField, TimestampType
+from pyspark.sql.functions import col, lit, to_timestamp
 
 
 consumption_metering_point_created_event_schema = StructType([
@@ -40,7 +41,8 @@ metering_point_base_schema = StructType([
     StructField("unit", StringType(), False),
     StructField("product", StringType(), False),
     StructField("settlement_method", StringType(), False),
-
+    StructField("valid_from", TimestampType(), False),
+    StructField("valid_to", TimestampType(), False),
 ])
 
 
@@ -49,15 +51,56 @@ metering_point_grid_area_schema = StructType([
     StructField("grid_area", StringType(), False),
     StructField("in_grid_area", StringType(), False),
     StructField("out_grid_area", StringType(), False),
+    StructField("valid_from", TimestampType(), False),
+    StructField("valid_to", TimestampType(), False),
 ])
 
 
 metering_point_connection_state_schema = StructType([
     StructField("metering_point_id", StringType(), False),
     StructField("connection_state", StringType(), False),
+    StructField("valid_from", TimestampType(), False),
+    StructField("valid_to", TimestampType(), False),
 ])
 
 
-def test_create_consumption_metering_point():
-    create_consumption_mp_event = [("1", "E17", "1234", "500", "D01", "D01", "P1H", "NVM", "23", datetime(2021, 1, 1))]
-    
+def test_create_consumption_metering_point(spark):
+    create_consumption_mp_event = [("1", "E17", "1234", "500", "D01", "D01", "P1H", "NVM", "23", datetime(2021, 1, 1, 0, 0))]
+
+    consumption_metering_point_event_df = spark.createDataFrame(create_consumption_mp_event, schema=consumption_metering_point_created_event_schema)
+
+    metering_point_base_df = consumption_metering_point_event_df \
+        .select("metering_point_id", "metering_point_type", "settlement_method", "meter_reading_periodicity", "product", "effective_date")
+
+    metering_point_base_df = metering_point_base_df \
+        .withColumn("parent_id", lit("")) \
+        .withColumn("unit", lit("kwh")) \
+        .withColumn("valid_to", lit(datetime(9999, 1, 1)).cast("timestamp")) \
+        .withColumnRenamed("meter_reading_periodicity", "resolution") \
+        .withColumnRenamed("effective_date", "valid_from")
+
+    metering_point_grid_area_df = consumption_metering_point_event_df \
+        .select("metering_point_id", "metering_grid_area", "effective_date")
+
+    metering_point_grid_area_df = metering_point_grid_area_df \
+        .withColumn("in_grid_area", lit("")) \
+        .withColumn("out_grid_area", lit("")) \
+        .withColumn("valid_to", lit(datetime(9999, 1, 1)).cast("timestamp")) \
+        .withColumnRenamed("effective_date", "valid_from")
+
+    metering_point_connection_state_df = consumption_metering_point_event_df \
+        .select("metering_point_id", "effective_date")
+
+    metering_point_connection_state_df = metering_point_connection_state_df \
+        .withColumn("connection_state", lit("")) \
+        .withColumn("valid_to", lit(datetime(9999, 1, 1)).cast("timestamp")) \
+        .withColumnRenamed("effective_date", "valid_from")
+
+    print(consumption_metering_point_event_df.show())
+    print(metering_point_base_df.show())
+    print(metering_point_grid_area_df.show())
+    print(metering_point_connection_state_df.show())
+
+    assert metering_point_base_df.collect()[0]["metering_point_id"] == "1"
+    # assert metering_point_grid_area.filter(col("metering_point_id") == "1")
+    # assert metering_point_connection_state.filter(col("metering_point_id") == "1")

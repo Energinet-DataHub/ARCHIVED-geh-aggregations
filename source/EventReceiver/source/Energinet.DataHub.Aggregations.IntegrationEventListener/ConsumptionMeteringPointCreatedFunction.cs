3747917d23e2db8baba9fbce42d7b7e8c78f22ce
Jonas Daugaard Møller <jdmsmail@gmail.com>
2021-10-04 15:33:54 +0200

    Renamed solution

---
+++ source/EventReceiver/source/Energinet.DataHub.Aggregations.IntegrationEventListener/ConsumptionMeteringPointCreatedFunction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Azure.Functions.Worker;
+
+namespace Energinet.DataHub.Aggregations
+{
+    public static class ConsumptionMeteringPointCreatedFunction
+    {
+        [Function("ConsumptionMeteringPointCreatedFunction")]
+        public static void Run(
+            [ServiceBusTrigger(
+                "consumption-metering-point-created",
+                "consumption-metering-point-created-sub-aggregations",
+                Connection = "ServiceBusConnection")] byte[] data,
+            FunctionContext context)
+        {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+            if (data == null) throw new ArgumentNullException(nameof(data));
+        }
+    }
+}
---
+++ source/EventReceiver/source/Energinet.DataHub.Aggregations.IntegrationEventListener/Program.cs
@@ -0,0 +1,58 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.IO;
+using GreenEnergyHub.Aggregation.Application;
+using GreenEnergyHub.Aggregation.Application.Interfaces;
+using GreenEnergyHub.Aggregation.Infrastruct;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Energinet.DataHub.Aggregations
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // wire up configuration
+            var host = new HostBuilder().ConfigureAppConfiguration(configurationBuilder =>
+                {
+                    configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    configurationBuilder.AddJsonFile("local.settings.json", true, true);
+                    configurationBuilder.AddEnvironmentVariables();
+                })
+                .ConfigureFunctionsWorkerDefaults();
+            //wire up DI
+            var buildHost = host.ConfigureServices((context, services) =>
+            {
+              // Setup Serilog
+                using var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
+                telemetryConfiguration.InstrumentationKey = context.Configuration["APPINSIGHTS_INSTRUMENTATIONKEY"];
+                var logger = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .WriteTo.ApplicationInsights(telemetryConfiguration, TelemetryConverter.Traces)
+                    .CreateLogger();
+
+                services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(logger));
+                services.AddSingleton<IEventDispatcher, EventDispatcher>();
+                services.AddSingleton<IEventHubService, EventHubService>();
+            }).Build();
+
+            buildHost.Run();
+        }
+    }
+}

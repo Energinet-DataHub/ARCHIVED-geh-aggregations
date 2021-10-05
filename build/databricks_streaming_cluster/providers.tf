d554769624a174ecda8539a76db463762f456493
Kristian Schneider <khs@thamesconsulting.com>
2021-10-05 11:39:25 +0200

    bump up TF DBR version

--- build/databricks_streaming_cluster/providers.tf
+++ build/databricks_streaming_cluster/providers.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     databricks = {
       source = "databrickslabs/databricks"
-      version = "0.2.8"
+      version = "0.3.7"
     }
     azurerm = "=2.31.1"
   }

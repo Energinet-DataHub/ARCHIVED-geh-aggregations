180b6f94c2674eb6bbe5b842171d6e45ca7b022c
Kristian Schneider <khs@thamesconsulting.com>
2021-10-07 13:00:57 +0200

    .

--- build/infrastructure/kv-aggregation.tf
+++ build/infrastructure/kv-aggregation.tf
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-data "azurerm_client_config" "main" {}
+data "azurerm_client_config" "current" {}
 
 module "kv_aggregation" {
   source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//key-vault?ref=1.2.0"

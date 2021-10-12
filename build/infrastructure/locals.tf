7b2df1505206e19d827506beb5b8e7a3c62fdb26
Kristian Schneider <khs@thamesconsulting.com>
2021-10-12 10:55:32 +0200

    Moved delta lake to shared

--- build/infrastructure/locals.tf
+++ build/infrastructure/locals.tf
@@ -11,9 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-locals {
-    master-data-blob-name = "master-data"
-    events-blob-name = "events"
-    results-blob-name = "results"
-    snapshots-blob-name = "snapshots"
-}

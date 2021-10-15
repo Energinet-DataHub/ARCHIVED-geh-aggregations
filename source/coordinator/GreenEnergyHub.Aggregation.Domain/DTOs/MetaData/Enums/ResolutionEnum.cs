2ba36bc8225373a3f27976e61b2e493ad9d405a4
Lasse Klitgaard <klitgaard84@gmail.com>
2021-10-15 12:15:01 +0200

    add resolution enum

---
+++ source/coordinator/GreenEnergyHub.Aggregation.Domain/DTOs/MetaData/Enums/ResolutionEnum.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace GreenEnergyHub.Aggregation.Domain.DTOs.MetaData.Enums
+{
+    public enum ResolutionEnum
+    {
+        [Description("15 minutes")]
+        Quarter = 0,
+        [Description("1 hour")]
+        Hour = 1,
+    }
+}

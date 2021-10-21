52673f46551e300dff54dcb6298113acfe80137b
Morten Kjær Nicolaysen <71763962+mknic@users.noreply.github.com>
2021-10-21 09:33:49 +0200

    use initialize_spark from data loader

--- source/databricks/streaming-jobs/streaming.py
+++ source/databricks/streaming-jobs/streaming.py
@@ -23,7 +23,7 @@ from pyspark.sql.session import SparkSession
 from geh_stream.streaming_utils.eventhub_ingestor import events_ingenstion_stream
 from geh_stream.streaming_utils.events_data_lake_listener import events_delta_lake_listener
 
-# TODO MRK issue #400: from geh_stream.shared.data_loader import initialize_spark
+from geh_stream.shared.data_loader import initialize_spark
 
 p = configargparse.ArgParser(description='Green Energy Hub events stream ingestor', formatter_class=configargparse.ArgumentDefaultsHelpFormatter)
 p.add('--data-storage-account-name', type=str, required=True)
@@ -35,16 +35,7 @@ p.add('--master-data-blob-name', type=str, required=True)
 
 args, unknown_args = p.parse_known_args()
 
-# TODO MRK issue #400: Use spark_initializer from data_loader located in shared: spark = initialize_spark(args)
-spark_conf = SparkConf(loadDefaults=True) \
-    .set('fs.azure.account.key.{0}.dfs.core.windows.net'.format(args.data_storage_account_name), args.data_storage_account_key) \
-    .set("spark.sql.session.timeZone", "UTC") \
-    .set("spark.databricks.io.cache.enabled", "True")
-
-spark = SparkSession \
-    .builder\
-    .config(conf=spark_conf)\
-    .getOrCreate()
+spark = initialize_spark(args)
 
 events_delta_path = f"abfss://{args.delta_lake_container_name}@{args.data_storage_account_name}.dfs.core.windows.net/{args.events_data_blob_name}"
 master_data_path = f"abfss://{args.delta_lake_container_name}@{args.data_storage_account_name}.dfs.core.windows.net/{args.master_data_blob_name}"

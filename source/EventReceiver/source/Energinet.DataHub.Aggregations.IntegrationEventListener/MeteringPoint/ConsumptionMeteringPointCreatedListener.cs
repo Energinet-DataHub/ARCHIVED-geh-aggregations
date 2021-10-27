e0644e051324dbfaae7b29d20464cb9c80af9352
Jonas Daugaard Møller <jdmsmail@gmail.com>
2021-10-27 12:42:47 +0200

    Added missing copyright disclaimer

--- source/EventReceiver/source/Energinet.DataHub.Aggregations.IntegrationEventListener/MeteringPoint/ConsumptionMeteringPointCreatedListener.cs
+++ source/EventReceiver/source/Energinet.DataHub.Aggregations.IntegrationEventListener/MeteringPoint/ConsumptionMeteringPointCreatedListener.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Energinet.DataHub.Aggregations.Application.Interfaces;
@@ -27,7 +41,7 @@ namespace Energinet.DataHub.Aggregations.MeteringPoint
             [ServiceBusTrigger(
                 "%CONSUMPTION_METERING_POINT_CREATED_TOPIC_NAME%",
                 "%CONSUMPTION_METERING_POINT_CREATED_SUBSCRIPTION_NAME%",
-                Connection = "INTEGRATIONEVENT_LISTENER_CONNECTION_STRING")] byte[] data,
+                Connection = "%INTEGRATIONEVENT_LISTENER_CONNECTION_STRING%")] byte[] data,
             FunctionContext context)
         {
             if (context == null) throw new ArgumentNullException(nameof(context));

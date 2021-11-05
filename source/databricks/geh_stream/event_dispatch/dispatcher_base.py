840b01010f1aa071ccfc284df009eb779037aabb
Lasse Klitgaard <lki@energinet.dk>
2021-11-05 12:58:55 +0100

    code cleanup

--- source/databricks/geh_stream/event_dispatch/dispatcher_base.py
+++ source/databricks/geh_stream/event_dispatch/dispatcher_base.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql.functions import col, when, lag, row_number, lead
+from pyspark.sql.functions import col, when, row_number, lead
 from geh_stream.codelists import Colname
 from pyspark.sql.window import Window
 
@@ -21,89 +21,93 @@ def period_mutations(spark, target_dataframe: DataFrame, event_df: DataFrame, co
 
     # Update col names to update on event dataframe
     for col_to_change in cols_to_change:
-        event_df = event_df.withColumnRenamed(col_to_change, f"updated_{col_to_change}")
-
-    event_df.show()
+        event_df = event_df \
+            .withColumnRenamed(col_to_change, f"updated_{col_to_change}")
 
     # Join event dataframe with target dataframe
-    joined = target_dataframe.join(event_df, [Colname.metering_point_id], "inner")
-    joined.show()
+    joined = target_dataframe \
+        .join(event_df, [Colname.metering_point_id], "inner")
 
     # periods that should not be updated
-    df_periods_to_keep = joined.filter(col(Colname.to_date) <= col(Colname.effective_date))
-    df_periods_to_keep.show()
+    df_periods_to_keep = joined \
+        .filter(col(Colname.to_date) <= col(Colname.effective_date))
 
     # Periods that should be updated
     df_periods_to_update = joined \
         .filter(col(Colname.to_date) > col(Colname.effective_date)) \
         .orderBy(col(Colname.from_date))
 
-    df_periods_to_update.show()
-
     periods_to_update_count = df_periods_to_update.count()
 
-    update_func_to_date = (when((col(Colname.from_date) < col(Colname.effective_date)) & (col(Colname.to_date) > col(Colname.effective_date)), col(Colname.effective_date))
-                           .otherwise(col(Colname.to_date)))
+    update_func_to_date = (
+        when(
+            (col(Colname.from_date) < col(Colname.effective_date))
+            & (col(Colname.to_date) > col(Colname.effective_date)),
+            col(Colname.effective_date))
+        .otherwise(col(Colname.to_date)))
 
     if periods_to_update_count > 1:
 
-        windowSpec = Window.partitionBy(Colname.metering_point_id).orderBy(Colname.to_date)
+        windowSpec = Window \
+            .partitionBy(Colname.metering_point_id) \
+            .orderBy(Colname.to_date)
 
-        df_periods_to_update = df_periods_to_update.withColumn("row_number", row_number().over(windowSpec))
-
-        print("after row number")
-        df_periods_to_update.show()
+        df_periods_to_update = df_periods_to_update \
+            .withColumn("row_number", row_number().over(windowSpec))
 
         df_periods_to_update = df_periods_to_update \
             .withColumn(Colname.to_date, update_func_to_date)
 
-        df_periods_to_update = df_periods_to_update.withColumn("next_from_date", lead(col(Colname.from_date), 1).over(windowSpec))
+        df_periods_to_update = df_periods_to_update \
+            .withColumn("next_from_date", lead(col(Colname.from_date), 1).over(windowSpec))
 
-        print("after to_date updated")
-        df_periods_to_update.show()
+        new_row = df_periods_to_update \
+            .filter(col("row_number") == 1)
 
-        new_row = df_periods_to_update.filter(col("row_number") == 1)
-
-        new_row = new_row.withColumn(Colname.from_date, col(Colname.to_date)).withColumn(Colname.to_date, col("next_from_date"))
+        new_row = new_row \
+            .withColumn(Colname.from_date, col(Colname.to_date)) \
+            .withColumn(Colname.to_date, col("next_from_date"))
 
         # Only add new_row if from_date and to_date are not equal
         if(new_row.filter(col(Colname.from_date) != col(Colname.to_date)).count() == 1):
-            df_periods_to_update = df_periods_to_update.union(new_row)
+            df_periods_to_update = df_periods_to_update \
+                .union(new_row)
 
-        print("after from_date updated")
-        df_periods_to_update.show()
-
-        df_periods_to_update = df_periods_to_update.drop("row_number")
-
-        print("after drop row_number")
-        df_periods_to_update.show()
+        df_periods_to_update = df_periods_to_update \
+            .drop("row_number")
 
     else:
-        df_periods_to_update = df_periods_to_update.withColumn("old_to_date", col(Colname.to_date))
-        df_periods_to_update = df_periods_to_update.withColumn(Colname.to_date, update_func_to_date)
+        df_periods_to_update = df_periods_to_update \
+            .withColumn("old_to_date", col(Colname.to_date)) \
+            .withColumn(Colname.to_date, update_func_to_date)
 
         new_row = df_periods_to_update \
             .withColumn(Colname.from_date, col(Colname.effective_date)) \
             .withColumn(Colname.to_date, col("old_to_date"))
 
-        df_periods_to_update = df_periods_to_update.union(new_row)
+        df_periods_to_update = df_periods_to_update \
+            .union(new_row)
 
-    split_period = df_periods_to_update.filter(col(Colname.to_date) == col(Colname.effective_date)).select(target_dataframe.columns)
-    df_periods_to_keep = df_periods_to_keep.select(target_dataframe.columns).union(split_period)
+    split_period = df_periods_to_update \
+        .filter(col(Colname.to_date) == col(Colname.effective_date)) \
+        .select(target_dataframe.columns)
 
-    df_periods_to_update = df_periods_to_update.filter(col(Colname.to_date) > col(Colname.effective_date))
+    df_periods_to_keep = df_periods_to_keep \
+        .select(target_dataframe.columns) \
+        .union(split_period)
+
+    df_periods_to_update = df_periods_to_update \
+        .filter(col(Colname.to_date) > col(Colname.effective_date))
 
     for col_to_change in cols_to_change:
         df_periods_to_update = df_periods_to_update \
             .withColumn(col_to_change, col(f"updated_{col_to_change}"))
 
-    print("after update")
-    df_periods_to_update.show()
+    df_periods_to_update = df_periods_to_update \
+        .select(target_dataframe.columns)
 
-    df_periods_to_update = df_periods_to_update.select(target_dataframe.columns)
-
-    result = df_periods_to_keep.select(target_dataframe.columns).union(df_periods_to_update)
-    print("result")
-    result.show()
+    result = df_periods_to_keep \
+        .select(target_dataframe.columns) \
+        .union(df_periods_to_update)
 
     return result

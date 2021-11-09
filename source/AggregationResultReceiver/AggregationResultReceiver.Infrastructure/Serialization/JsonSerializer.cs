6c112c1a4fa284b527ae0f66e759a7025d10fa58
Jonas Daugaard Møller <jdmsmail@gmail.com>
2021-11-09 10:15:41 +0100

    Copied code from IntegrationEventListener solution

---
+++ source/AggregationResultReceiver/AggregationResultReceiver.Infrastructure/Serialization/JsonSerializer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using AggregationResultReceiver.Application.Serialization;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace AggregationResultReceiver.Infrastructure.Serialization
+{
+    public class JsonSerializer : IJsonSerializer
+    {
+        private readonly JsonSerializerOptions _options;
+
+        public JsonSerializer()
+        {
+            _options = new JsonSerializerOptions();
+            _options.Converters.Add(NodaConverters.InstantConverter);
+        }
+
+        public ValueTask<object?> DeserializeAsync(Stream utf8Json, Type returnType)
+        {
+            if (utf8Json == null) throw new ArgumentNullException(nameof(utf8Json));
+            return System.Text.Json.JsonSerializer.DeserializeAsync(utf8Json, returnType, _options);
+        }
+
+        public TValue? Deserialize<TValue>(string json)
+        {
+            if (json == null) throw new ArgumentNullException(nameof(json));
+            return System.Text.Json.JsonSerializer.Deserialize<TValue>(json, _options);
+        }
+
+        public object? Deserialize(string json, Type returnType)
+        {
+            if (json == null) throw new ArgumentNullException(nameof(json));
+            return System.Text.Json.JsonSerializer.Deserialize(json, returnType, _options);
+        }
+
+        public string Serialize<TValue>(TValue value)
+        {
+            if (value == null) throw new ArgumentNullException(nameof(value));
+            return System.Text.Json.JsonSerializer.Serialize<object>(value, _options);
+        }
+    }
+}
--- source/AggregationResultReceiver/AggregationResultReceiver.Tests/CimXmlResultSerializerTests.cs
+++ source/AggregationResultReceiver/AggregationResultReceiver.Tests/CimXmlResultSerializerTests.cs
@@ -1,12 +1,61 @@
-﻿using Xunit;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Threading.Tasks;
+using AggregationResultReceiver.Domain;
+using AggregationResultReceiver.Infrastructure.CimXml;
+using Energinet.DataHub.AggregationResultReceiver.Tests.Attributes;
+using Xunit;
 using Xunit.Categories;
 
-namespace AggregationResultReceiver.Tests
+namespace Energinet.DataHub.AggregationResultReceiver.Tests
 {
     [UnitTest]
     public class CimXmlResultSerializerTests
     {
+        private const int NoOfResultsInBundle = 10;
+
         [Theory]
-        [InlineData("")]
+        [InlineAutoMoqData("Assets/ExpectedAggregationResultForFlexConsumptionPerGridAreaMdr.blob")]
+        public async Task SerializeToStreamAsync_ValidInput_ReturnsCorrectsXml(
+            string expected,
+            [NotNull] CimXmlResultSerializer sut)
+        {
+            var input = GetResultData();
+            await using var actualStream = new MemoryStream();
+            await sut.SerializeToStreamAsync(input, actualStream);
+            var actual = actualStream.ToString();
+            Assert.Equal(expected, actual);
+        }
+
+        private static IEnumerable<ResultData> GetResultData()
+        {
+            // Læs JSON
+            // Deserialiser json -> List<ResultData>
+            var resultData = new List<ResultData>();
+
+            for (var i = 0; i < NoOfResultsInBundle; i++)
+            {
+                resultData.Add(new ResultData(
+                    "JobId",
+                    "SnapshotId",
+                    "ResultId",
+                    "ResultName",
+                    "GridArea",
+                    "InGridArea",
+                    "OutGridArea",
+                    "BalanceResponsibleId",
+                    "EnergySupplierId",
+                    "StartDateTime",
+                    "EndDateTime",
+                    "Resolution",
+                    12.34M,
+                    "MeteringPointType",
+                    "SettlementMethod"));
+            }
+
+            return resultData;
+        }
     }
 }

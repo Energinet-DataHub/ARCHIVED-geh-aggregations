bb929e198d75aabf8c1fa088ff361e60d42552db
Lasse Klitgaard <lki@energinet.dk>
2021-11-09 09:19:00 +0100

    rename variable

--- source/databricks/geh_stream/event_dispatch/meteringpoint_dispatcher.py
+++ source/databricks/geh_stream/event_dispatch/meteringpoint_dispatcher.py
@@ -40,12 +40,12 @@ def on_settlement_method_updated(msg: m.SettlementMethodUpdated):
     master_data_path = f"{dispatcher.master_data_root_path}{msg.get_master_data_path}"
 
     # Get all existing metering point periods
-    consumption_mps_df = spark.read.format("delta").load(master_data_path).where(f"{Colname.metering_point_id} = '{msg.metering_point_id}'")
+    mps_df = spark.read.format("delta").load(master_data_path).where(f"{Colname.metering_point_id} = '{msg.metering_point_id}'")
 
     # Get the event data frame
     settlement_method_updated_df = msg.get_dataframe()
 
-    result_df = period_mutations(consumption_mps_df, settlement_method_updated_df, [Colname.settlement_method])
+    result_df = period_mutations(mps_df, settlement_method_updated_df, [Colname.settlement_method])
 
     # persist updated mps
     result_df \

bea13acec22ac04e562a7e26b735efaaf97d591c
Kristian Schneider <khs@thamesconsulting.com>
2021-11-19 10:00:40 +0100

    tweaked entrypoint.sh to run coverage

--- .github/actions/databricks-unit-test/entrypoint.sh
+++ .github/actions/databricks-unit-test/entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -l
 
-cd ./source/databricks
+cd ./source/databricks/tests/
+#Build wheel
 python setup.py install
-# Generate Python classes from ProtoBuf contracts
-python -m pip install -e ./
 # python coverage-threshold install
 pip install coverage-threshold
-coverage run --branch -m pytest tests/
+coverage run --branch -m pytest .
 # Create data for threshold evaluation
 coverage json
 # Create human reader friendly HTML report

41d561f9dd21ae1cbc29162eecc5148abd83ad4d
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2021-11-23 11:58:04 +0100

    try to remove version number from wheel artifact

--- source/databricks/setup.py
+++ source/databricks/setup.py
@@ -14,12 +14,12 @@
 from setuptools import setup, find_packages
 
 # File 'VERSION' is created by pipeline. If executed manual it must be created manually.
-__version__ = ""
-with open('VERSION') as version_file:
-    __version__ = version_file.read().strip()
+#__version__ = ""
+#with open('VERSION') as version_file:
+#    __version__ = version_file.read().strip()
 
 setup(name='geh_stream',
-      version=__version__,
+#      version=__version__,
       description='Tools for streaming and aggregation of meter data of Green Energy Hub',
       long_description="",
       long_description_content_type='text/markdown',

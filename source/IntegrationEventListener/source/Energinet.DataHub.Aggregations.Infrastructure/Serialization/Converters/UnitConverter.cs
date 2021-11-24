2ff927cd12dbb22d17a1a3990ece647365c93d65
Morten Kjær Nicolaysen <71763962+mknic@users.noreply.github.com>
2021-11-24 12:26:36 +0100

    Simplify UnitConverter by using the new enum extension method

--- source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.Infrastructure/Serialization/Converters/UnitConverter.cs
+++ source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.Infrastructure/Serialization/Converters/UnitConverter.cs
@@ -15,6 +15,7 @@
 using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Energinet.DataHub.Aggregations.Application.Extensions;
 using Energinet.DataHub.Aggregations.Domain;
 
 namespace Energinet.DataHub.Aggregations.Infrastructure.Serialization.Converters
@@ -24,15 +25,19 @@ namespace Energinet.DataHub.Aggregations.Infrastructure.Serialization.Converters
         public override Unit Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
         {
             var value = reader.GetString();
-            if (value != "KWH") throw new ArgumentOutOfRangeException(nameof(value), value, "Could not read JSON value");
-            return Unit.Kwh;
+
+            return value.GetEnumValueFromAttribute<Unit>();
         }
 
         public override void Write(Utf8JsonWriter writer, Unit value, JsonSerializerOptions options)
         {
-            if (writer == null) throw new ArgumentNullException(nameof(writer));
-            if (value != Unit.Kwh) throw new ArgumentOutOfRangeException(nameof(value), value, "Could not write JSON value");
-            writer.WriteStringValue("KWH");
+            if (writer == null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            var description = value.GetDescription();
+            writer.WriteStringValue(description);
         }
     }
 }

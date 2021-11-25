ed4280efaec4d96abcab898f59f0d2efafc1e83d
Jonas Michelsen <jvm@energinet.dk>
2021-11-25 12:39:14 +0100

    Remove unused DataResult class

--- source/AggregationResultReceiver/source/AggregationResultReceiver.Domain/ResultData.cs
+++ source/AggregationResultReceiver/source/AggregationResultReceiver.Domain/ResultData.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Newtonsoft.Json;
-
 namespace Energinet.DataHub.Aggregations.AggregationResultReceiver.Domain
 {
 #pragma warning disable SA1313

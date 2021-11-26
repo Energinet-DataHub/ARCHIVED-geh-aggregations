6fbab335f36a708ff4545f101980bdbe32135a03
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2021-11-26 13:50:19 +0100

    remove unnecessary stuff and fix indentation

--- build/infrastructure/main/sqldb-this.tf
+++ build/infrastructure/main/sqldb-this.tf
@@ -11,11 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# locals {
-#   metadataDatabaseAdminName = "metadataadmin"
-# }
-
 data "azurerm_sql_server" "sqlsrv" {
   name                = data.azurerm_key_vault_secret.sql_data_name.value
   resource_group_name = data.azurerm_resource_group.shared_resources.name

6b6f5665b811aca76be0811b0478626cb1e61ec6
Jonas Michelsen <jvm@energinet.dk>
2021-11-29 09:54:42 +0100

    Summary

--- source/AggregationResultReceiver/source/AggregationResultReceiver.Application/IFileStore.cs
+++ source/AggregationResultReceiver/source/AggregationResultReceiver.Application/IFileStore.cs
@@ -28,7 +28,7 @@ namespace Energinet.DataHub.Aggregations.AggregationResultReceiver.Application
         Task UploadConvertedMessageAsync(string fileName, Stream content);
 
         /// <summary>
-        /// Fetch aggregation result
+        /// Fetch Blob
         /// </summary>
         Task<Stream> DownloadBlobAsync(string fileName);
     }

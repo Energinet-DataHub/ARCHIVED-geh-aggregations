dd040e93db86ecaea047bc5132ffdf589655e8d8
Lasse Klitgaard <lki@energinet.dk>
2021-12-02 13:57:00 +0100

    add paths to delta tables in coordinator settings

--- source/coordinator/GreenEnergyHub.Aggregation.Application/Coordinator/CoordinatorSettings.cs
+++ source/coordinator/GreenEnergyHub.Aggregation.Application/Coordinator/CoordinatorSettings.cs
@@ -38,7 +38,21 @@ namespace GreenEnergyHub.Aggregation.Application.Coordinator
 
         public string TimeSeriesPath { get; set; }
 
-        public string PersistLocation { get; set; }
+        public string MeteringPointsPath { get; set; }
+
+        public string MarketRolesPath { get; set; }
+
+        public string ChargesPath { get; set; }
+
+        public string ChargeLinksPath { get; set; }
+
+        public string ChargePricesPath { get; set; }
+
+        public string EsBrpRelationsPath { get; set; }
+
+        public string GridLossSystemCorrectionPath { get; set; }
+
+        public string SnapshotPath { get; set; }
 
         public string AggregationPythonFile { get; set; }
 

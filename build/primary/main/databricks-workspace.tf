b288e82e70d53d09239532a2b9a43482869fef8c
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-02-10 12:01:53 +0100

    fix TF module refs

--- build/primary/main/databricks-workspace.tf
+++ build/primary/main/databricks-workspace.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "dbw_aggregations" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/databricks-workspace?ref=6.0.0-add-databricks-workspace-module"
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/databricks-workspace?ref=6.0.0"
 
   name                                      = "dbw"
   project_name                              = var.domain_name_short

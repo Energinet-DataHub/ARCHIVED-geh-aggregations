c8eaf971fd7765762587bc07dfbb09ac8814123c
Kristian Schneider <khs@thamesconsulting.com>
2022-02-15 10:46:07 +0100

    .

--- build/primary/main/kv-shared-resources.tf
+++ build/primary/main/kv-shared-resources.tf
@@ -51,16 +51,6 @@ data "azurerm_key_vault_secret" "st_shared_data_lake_name" {
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
 
-data "azurerm_key_vault_secret" "st_shared_data_lake_data_container_name" {
-  name         = "st-data-lake-data-container-name"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
-data "azurerm_key_vault_secret" "st_shared_data_lake_timeseries_blob_name" {
-  name         = "st-data-lake-timeseries-blob-name"
-  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
-}
-
 data "azurerm_key_vault_secret" "st_shared_data_lake_primary_access_key" {
   name         = "st-data-lake-primary-access-key"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id

f983cb1a293c698bf894a53dcdb2ad3d0e4b9bf8
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-03-03 13:34:33 +0100

    use 0.4.0 databricks providers

--- build/databricks_streaming_cluster/main/providers.tf
+++ build/databricks_streaming_cluster/main/providers.tf
@@ -17,7 +17,7 @@ terraform {
   required_providers {
     databricks = {
       source = "databrickslabs/databricks"
-      version = "0.5.1"
+      version = "0.4.0"
     }
     azurerm = "=2.91.0"
   }

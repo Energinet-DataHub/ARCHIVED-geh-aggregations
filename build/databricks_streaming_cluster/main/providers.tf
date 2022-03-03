58790f108003a1741bd6ff878670076d52ddb5b9
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-03-03 14:16:02 +0100

    use newer TF, azurerm, databrickslabs version

--- build/databricks_streaming_cluster/main/providers.tf
+++ build/databricks_streaming_cluster/main/providers.tf
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 terraform {
-  required_version = "1.1.5"
+  required_version = "1.1.6"
 
   required_providers {
     databricks = {
       source = "databrickslabs/databricks"
-      version = "0.3.8"
+      version = "0.5.0"
     }
-    azurerm = "=2.91.0"
+    azurerm = "=2.98.0"
   }
 }
 

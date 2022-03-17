ccab6d4bd15ca357ca8ec5a0733f8612d40e2d30
Lasse Klitgaard <lki@energinet.dk>
2022-03-17 19:52:43 +0100

    use earlier version for keyvault terraform module

--- build/primary/main/kv-aggregation.tf
+++ build/primary/main/kv-aggregation.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "kv_aggregations" {
-  source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault?ref=5.6.0"
+  source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault?ref=5.1.0"
 
   name                            = "aggrgtn"
   project_name                    = var.domain_name_short

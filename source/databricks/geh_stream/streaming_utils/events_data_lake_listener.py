4904f968f7e87761b1c7a6432505e06e2ca7c29a
Kristian Schneider <khs@thamesconsulting.com>
2022-03-22 12:59:07 +0100

    removed local await

--- source/databricks/geh_stream/streaming_utils/events_data_lake_listener.py
+++ source/databricks/geh_stream/streaming_utils/events_data_lake_listener.py
@@ -38,6 +38,4 @@ def events_delta_lake_listener(delta_lake_container_name: str, storage_account_n
 
     dispatcher.set_master_data_root_path(master_data_path)
 
-    stream = inputDf.writeStream.option("checkpointLocation", checkpoint_path).foreachBatch(lambda df, epochId: incomming_event_handler(df, epochId)).start()
-
-    stream.awaitTermination()
+    inputDf.writeStream.option("checkpointLocation", checkpoint_path).foreachBatch(lambda df, epochId: incomming_event_handler(df, epochId)).start()

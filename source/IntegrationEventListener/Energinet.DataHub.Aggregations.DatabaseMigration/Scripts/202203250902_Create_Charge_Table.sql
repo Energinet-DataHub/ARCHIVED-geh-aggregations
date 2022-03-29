53d96a2f61492837f1ef3cb5209ac9f19d0ff74c
Lasse Klitgaard <lki@energinet.dk>
2022-03-29 14:51:52 +0200

    Revert "revert"
    This reverts commit 81f04064f35e2dd917ac728f609bdc2c57594859.

--- source/IntegrationEventListener/Energinet.DataHub.Aggregations.DatabaseMigration/Scripts/202203250902_Create_Charge_Table.sql
+++ source/IntegrationEventListener/Energinet.DataHub.Aggregations.DatabaseMigration/Scripts/202203250902_Create_Charge_Table.sql
@@ -1,17 +1,10 @@
 ﻿CREATE TABLE [dbo].[Charge]
 (
 	[RowId] [uniqueidentifier] NOT NULL,
-<<<<<<< HEAD
 	[Id] [nvarchar](255) NOT NULL,
 	[SenderProvidedChargeId] [nvarchar](max) NOT NULL,
 	[Type] [int] NOT NULL,
 	[OwnerId] [nvarchar](255) NOT NULL,
-=======
-	[ChargeKey] [nvarchar](max) NOT NULL,
-	[ChargeId] [nvarchar](255) NOT NULL,
-	[ChargeType] [int] NOT NULL,
-	[ChargeOwnerId] [nvarchar](255) NOT NULL,
->>>>>>> parent of 71eea6f3 (Update source/IntegrationEventListener/Energinet.DataHub.Aggregations.DatabaseMigration/Scripts/202203250902_Create_Charge_Table.sql)
 	[Resolution] [int] NOT NULL,
 	[TaxIndicator] [bit] NOT NULL,
 	[Currency] [nvarchar](50) NOT NULL,

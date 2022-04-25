4dd56f926dcbd3bc1e467fd98025356cf397c27d
Lasse Klitgaard <lki@energinet.dk>
2022-04-25 14:04:03 +0200

    change repo

--- source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.Infrastructure/Repository/MeteringPointRepository.cs
+++ source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.Infrastructure/Repository/MeteringPointRepository.cs
@@ -21,9 +21,12 @@ using Dapper;
 using Energinet.DataHub.Aggregations.Application.Extensions;
 using Energinet.DataHub.Aggregations.Domain;
 using Energinet.DataHub.Aggregations.Domain.MasterData;
+using Energinet.DataHub.Aggregations.Infrastructure.Persistence;
 using Energinet.DataHub.Aggregations.Infrastructure.Repository.InsertUpdates;
 using Energinet.DataHub.Aggregations.Infrastructure.Repository.SqlInstructions;
+using Microsoft.EntityFrameworkCore;
 using NodaTime;
+using MeteringPoint = Energinet.DataHub.Aggregations.Infrastructure.Persistence.MeteringPoint;
 
 namespace Energinet.DataHub.Aggregations.Infrastructure.Repository
 {
@@ -34,86 +37,98 @@ namespace Energinet.DataHub.Aggregations.Infrastructure.Repository
     /// </summary>
     public class MeteringPointRepository : IMasterDataRepository<MeteringPoint>
     {
-        private readonly string _connectionString;
-        private readonly IDictionary<Type, ISqlInstructions<IMasterDataObject>> _sqlInstructions;
+        private readonly MasterDataContext _dbContext;
 
-        public MeteringPointRepository(string connectionString)
+        public MeteringPointRepository(MasterDataContext dbContext)
         {
-            _connectionString = connectionString;
-            _sqlInstructions = new Dictionary<Type, ISqlInstructions<IMasterDataObject>>();
-
-            // Insert new master data manipulations to the list here
-            _sqlInstructions.Add(typeof(MeteringPoint), new MeteringPointSqlInstructions<MeteringPoint>());
+            _dbContext = dbContext;
         }
 
         public async Task<List<MeteringPoint>> GetByIdAndDateAsync(string id, Instant effectiveDate)
         {
-            await using var conn = await GetConnectionAsync().ConfigureAwait(false);
+            var mp = _dbContext.MeteringPoint.Where(x =>
+                x.MeteringPointId == id && x.ToDate > effectiveDate.ToDateTimeUtc());
 
-            var sqlInstructions = _sqlInstructions[typeof(MeteringPoint)];
+            return await mp.ToListAsync().ConfigureAwait(false);
 
-            var results = await conn
-                .QueryAsync<MeteringPoint>(
-                    sqlInstructions.Get,
-                    new { id, effectiveDate = effectiveDate.ToIso8601GeneralString() }).ConfigureAwait(false);
-            return results.ToList();
+            //await using var conn = await GetConnectionAsync().ConfigureAwait(false);
+
+            //var sqlInstructions = _sqlInstructions[typeof(MeteringPoint)];
+
+            //var results = await conn
+            //    .QueryAsync<MeteringPoint>(
+            //        sqlInstructions.Get,
+            //        new { id, effectiveDate = effectiveDate.ToIso8601GeneralString() }).ConfigureAwait(false);
+            //return results.ToList();
         }
 
         public async Task AddOrUpdateAsync(List<MeteringPoint> masterDataObjects)
         {
             if (masterDataObjects == null) throw new ArgumentNullException(nameof(masterDataObjects));
-
-            await using var conn = await GetConnectionAsync().ConfigureAwait(false);
-
-            foreach (var masterData in masterDataObjects)
+            foreach (var masterDataObject in masterDataObjects)
             {
-                if (await MasterDataExistsAsync<MeteringPoint>(masterData.RowId, conn).ConfigureAwait(false))
+                if (_dbContext.MeteringPoint.SingleOrDefault(x => x.RowId == masterDataObject.RowId) != null)
                 {
-                    await UpdateMasterDataObjectAsync(masterData, conn).ConfigureAwait(false);
+                    await InsertMasterDataAsync(masterDataObject).ConfigureAwait(false);
                 }
                 else
                 {
-                    await InsertMasterDataAsync(masterData, conn).ConfigureAwait(false);
+                    await UpdateMasterDataObjectAsync(masterDataObject).ConfigureAwait(false);
                 }
             }
+
+            //await using var conn = await GetConnectionAsync().ConfigureAwait(false);
+
+            //foreach (var masterData in masterDataObjects)
+            //{
+            //    if (await MasterDataExistsAsync<MeteringPoint>(masterData.RowId, conn).ConfigureAwait(false))
+            //    {
+            //        await UpdateMasterDataObjectAsync(masterData, conn).ConfigureAwait(false);
+            //    }
+            //    else
+            //    {
+            //        await InsertMasterDataAsync(masterData, conn).ConfigureAwait(false);
+            //    }
+            //}
         }
 
-        private static async Task<bool> MasterDataExistsAsync<T>(Guid rowId, SqlConnection conn)
-            where T : IMasterDataObject
+        //private static async Task<bool> MasterDataExistsAsync<T>(Guid rowId, SqlConnection conn)
+        //    where T : IMasterDataObject
+        //{
+        //    var count = await conn.ExecuteScalarAsync<int>(
+        //        $"SELECT COUNT(1) FROM dbo.{typeof(T).Name} md WHERE md.RowId = @rowId",
+        //        new { rowId }).ConfigureAwait(false);
+
+        //    return count > 0;
+        //}
+        private async Task InsertMasterDataAsync(MeteringPoint masterDataObject)
         {
-            var count = await conn.ExecuteScalarAsync<int>(
-                $"SELECT COUNT(1) FROM dbo.{typeof(T).Name} md WHERE md.RowId = @rowId",
-                new { rowId }).ConfigureAwait(false);
+            _dbContext.MeteringPoint.Add(masterDataObject);
+            await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+            //await using var transaction = await conn.BeginTransactionAsync().ConfigureAwait(false);
+            //var insertUpdateInstructions = _sqlInstructions[typeof(T)];
 
-            return count > 0;
+            //await conn.ExecuteAsync(insertUpdateInstructions.Insert, transaction: transaction, param: insertUpdateInstructions.InsertParameters(masterDataObject)).ConfigureAwait(false);
+            //await transaction.CommitAsync().ConfigureAwait(false);
         }
 
-        private async Task InsertMasterDataAsync<T>(T masterDataObject, SqlConnection conn)
-            where T : IMasterDataObject
+        private async Task UpdateMasterDataObjectAsync(MeteringPoint masterDataObject)
         {
-            await using var transaction = await conn.BeginTransactionAsync().ConfigureAwait(false);
-            var insertUpdateInstructions = _sqlInstructions[typeof(T)];
+            _dbContext.MeteringPoint.Update(masterDataObject);
+            await _dbContext.SaveChangesAsync().ConfigureAwait(false);
+            //await using var transaction = await conn.BeginTransactionAsync().ConfigureAwait(false);
 
-            await conn.ExecuteAsync(insertUpdateInstructions.Insert, transaction: transaction, param: insertUpdateInstructions.InsertParameters(masterDataObject)).ConfigureAwait(false);
-            await transaction.CommitAsync().ConfigureAwait(false);
+            //var insertUpdateInstructions = _sqlInstructions[typeof(T)];
+
+            //await conn.ExecuteAsync(insertUpdateInstructions.Update, transaction: transaction, param: insertUpdateInstructions.UpdateParameters(masterDataObject)).ConfigureAwait(false);
+            //await transaction.CommitAsync().ConfigureAwait(false);
         }
 
-        private async Task UpdateMasterDataObjectAsync<T>(T masterDataObject, SqlConnection conn)
-            where T : IMasterDataObject
-        {
-            await using var transaction = await conn.BeginTransactionAsync().ConfigureAwait(false);
-
-            var insertUpdateInstructions = _sqlInstructions[typeof(T)];
-
-            await conn.ExecuteAsync(insertUpdateInstructions.Update, transaction: transaction, param: insertUpdateInstructions.UpdateParameters(masterDataObject)).ConfigureAwait(false);
-            await transaction.CommitAsync().ConfigureAwait(false);
-        }
-
-        private async Task<SqlConnection> GetConnectionAsync()
-        {
-            var conn = new SqlConnection(_connectionString);
-            await conn.OpenAsync().ConfigureAwait(false);
-            return conn;
-        }
+        //private async Task<SqlConnection> GetConnectionAsync()
+        //{
+        //    var conn = new SqlConnection(_connectionString);
+        //    await conn.OpenAsync().ConfigureAwait(false);
+        //    return conn;
+        //}
     }
 }
--- source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.IntegrationEventListener/Program.cs
+++ source/IntegrationEventListener/source/Energinet.DataHub.Aggregations.IntegrationEventListener/Program.cs
@@ -19,18 +19,20 @@ using Energinet.DataHub.Aggregations.Application.IntegrationEvents.Mutators;
 using Energinet.DataHub.Aggregations.Common;
 using Energinet.DataHub.Aggregations.Configuration;
 using Energinet.DataHub.Aggregations.Domain;
-using Energinet.DataHub.Aggregations.Domain.MasterData;
 using Energinet.DataHub.Aggregations.Infrastructure.Messaging.Registration;
 using Energinet.DataHub.Aggregations.Infrastructure.Middleware;
+using Energinet.DataHub.Aggregations.Infrastructure.Persistence;
 using Energinet.DataHub.Aggregations.Infrastructure.Repository;
 using Energinet.DataHub.Core.App.FunctionApp.Middleware;
 using Energinet.DataHub.Core.App.FunctionApp.Middleware.CorrelationId;
 using Energinet.DataHub.Core.JsonSerialization;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using MeteringPoint = Energinet.DataHub.Aggregations.Domain.MasterData.MeteringPoint;
 
 namespace Energinet.DataHub.Aggregations
 {
@@ -72,8 +74,10 @@ namespace Energinet.DataHub.Aggregations
                 services.AddSingleton<IJsonSerializer, JsonSerializer>();
                 services.AddSingleton<EventDataHelper>();
 
-                services.AddSingleton<IMasterDataRepository<MeteringPoint>>(x =>
-                    new MeteringPointRepository(context.Configuration[EnvironmentSettingNames.MasterDataDbConString]));
+                services.AddScoped<IMasterDataRepository<MeteringPoint>, MeteringPointRepository>();
+
+                services.AddDbContext<MasterDataContext>(options =>
+                    options.UseSqlServer(context.Configuration[EnvironmentSettingNames.MasterDataDbConString]));
 
                 SetupMutators(services);
 

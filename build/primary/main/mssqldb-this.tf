488f3db7da9d1202cfddc6e9d04f0cebe07f7e7d
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-05-09 10:00:27 +0200

    fix TF

--- build/primary/main/mssqldb-this.tf
+++ build/primary/main/mssqldb-this.tf
@@ -24,18 +24,20 @@ module "mssqldb_aggregations" {
   environment_short           = var.environment_short
   environment_instance        = var.environment_instance
   server_id                   = data.azurerm_mssql_server.mssqlsrv.id
+  log_analytics_workspace_id  = data.azurerm_key_vault_secret.log_shared_id.value
   
   tags                        = azurerm_resource_group.this.tags
 }
 
 module "mssqldb_aggregations_masterdata" {
-  source                      = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=5.6.0"
+  source                      = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/mssql-database?ref=6.0.0"
 
   name                        = "master-data"
   project_name                = var.domain_name_short
   environment_short           = var.environment_short
   environment_instance        = var.environment_instance
   server_id                   = data.azurerm_mssql_server.mssqlsrv.id
+  log_analytics_workspace_id  = data.azurerm_key_vault_secret.log_shared_id.value
   
   tags                        = azurerm_resource_group.this.tags
 }
